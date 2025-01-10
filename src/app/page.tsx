b35cb268d7e68cc4ba91fa3f9cf2894c1f972385
Vocatann <ivane.lukanov@gmail.com>
2025-01-10 19:15:52 -0300

    refactor: wrote basic html for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div>
-      
+      <div className="w-1/2 mx-auto bg-blue-500">
+        <header className="px-3">
+          <button>
+            Reset
+          </button>
+        </header>
+      </div>
     </div>
   );
 }
+
