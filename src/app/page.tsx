617953dc5a2141af8f556e34d2af2b09c6a3059e
Vocatann <ivane.lukanov@gmail.com>
2025-01-14 15:38:54 -0300

    feat: added MainPage component and ItemsList component with basic deletion and rendering of items logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
+import MainPage from "@/components/MainPage";
+
 export default function Home() {
+
   return (
-    <div>
-      <div className="w-1/2 mx-auto bg-blue-500">
-        <header className="px-3">
-          <button>
-            Reset
-          </button>
-        </header>
-      </div>
-    </div>
+    <>
+      <MainPage/>
+    </>
   );
 }
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import ItemsList from "./ItemsList";
+
+const MainPage = () => {
+  const [itemIds, setItemIds] = useState<number[]>([1, 2, 3]);
+
+  const removeItem = (itemId: number) => {
+    setItemIds((prevItemIds) => prevItemIds.filter(id => id !== itemId));
+  };
+
+  return (
+    <div className="w-1/2 mx-auto bg-blue-500">
+      <header className="px-3">
+        <button>
+          Reset
+        </button>
+      </header>
+      <ItemsList itemIds={itemIds} mcVersion="1.19" onRemoveItem={removeItem}/>
+    </div>
+  );
+}
+
+export default MainPage;
