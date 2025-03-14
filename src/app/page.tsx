2031b76921359632567a2dceb9c6609fdbb744ae
Vocatann <ivane.lukanov@gmail.com>
2025-03-13 22:01:33 -0300

    feat: moves MainPage s code to paga

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import MainPage from "@/components/MainPage";
+"use client";
+
+import { useState } from "react";
+import ItemsList from "@/components/ItemsList";
 
 export default function Home() {
+  const [itemIds, setItemIds] = useState<number[]>([1, 2, 3]);
+
+  const removeItem = (itemId: number) => {
+    setItemIds((prevItemIds) => prevItemIds.filter(id => id !== itemId));
+  };
 
   return (
-    <>
-      <MainPage/>
-    </>
+    <div className="w-1/2 mx-auto bg-blue-500">
+      <header className="px-3">
+        <button onClick={() => setItemIds([])}>
+          Reset
+        </button>
+      </header>
+      <ItemsList itemIds={itemIds} mcVersion="1.21.3" onRemoveItem={removeItem}/>
+    </div>
   );
 }
---
+++ src/pages/api/items/[itemId].ts
@@ -0,0 +1,6 @@
+import type { NextApiResponse, NextApiRequest } from 'next';
+import minecraftData from 'minecraft-data';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+}
