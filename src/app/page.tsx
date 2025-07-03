f8df6fb883d09c98fd89bb9d2ad8c1559a826f89
Vocatann <ivane.lukanov@gmail.com>
2025-07-03 16:26:31 -0400

    feat(layout): add svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import ItemsList from "@/components/ItemsList";
 
+import { arrowPathSVG } from "@/utils/svgs";
+
 export default function Home() {
   const [itemIds, setItemIds] = useState<number[]>([1, 2, 3]);
 
@@ -27,11 +29,21 @@ export default function Home() {
         <h1 className="text-xl md:text-2xl lg:text-3xl text-center  w-1/2 lg:w-1/3">Minecraft Todo List</h1>
       </header>
       <div className="w-1/2 mx-auto mt-20">
-        <section className="border-2 rounded-sm border-border p-3">
-          <ul>
+        <section className="border-2 rounded-sm border-border">
+          <ul className="border-b-2 border-border flex items-center">
             <li>
-              <button onClick={() => setItemIds([])}>
-                Reset
+              <button
+                onClick={() => setItemIds([])}
+                className="w-auto px-2 h-10 flex items-center justify-center border-r-2 border-border hover:bg-border"
+              >
+                {arrowPathSVG}
+              </button>
+            </li>
+            <li>
+              <button
+                className="w-auto px-2 h-10 flex items-center justify-center border-r-2 border-border hover:bg-border"
+              >
+                metric
               </button>
             </li>
           </ul>
